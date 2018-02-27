9847c13302ca94000afcb51ea19d33c744e0efb5
Laís Tomaz <lais@eokoe.com>
2018-02-27 13:45:32 -0300

    Remove load vacancy from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { firebaseApp } from '../firebase';
 
 Vue.use(Vuex);
 
@@ -8,7 +7,6 @@ const store = new Vuex.Store({
 	state: {
 		eokoeUser: {},
 		candidate: '',
-		vacancy: {},
 	},
 	actions: {
 		SAVE_EOKOE_USER({ commit }, data) {
@@ -20,34 +18,10 @@ const store = new Vuex.Store({
 		SAVE_CANDIDATE({ commit }, data) {
 			commit('SET_CANDIDATE', { data });
 		},
-		// eslint-disable-next-line
-		LOAD_VACANCY({ commit }, id) {
-			const vacancySnapshot = firebaseApp.database().ref(`vacancies/${id}`);
-			vacancySnapshot.once('value').then((snapshot) => {
-				const vacancy = snapshot.val();
-				console.log('vaga', vacancy);
-				setTimeout(() => {
-					if (vacancy) {
-						console.log('tem');
-						commit('SET_VACANCY', { vacancy });
-					} else {
-						console.log('nao tem');
-						commit('SET_VACANCY', { erro: 'caralho' });
-					}
-				}, 1000);
-			});
-		},
 	},
 	mutations: {
 		SET_EOKOE_USER(state, { data }) {
 			state.eokoeUser = data;
-		},
-		SET_VACANCY(state, { data }) {
-			console.log('data', data);
-			state.vacancy = {
-				area: 'front',
-				level: 'junior',
-			};
 		},
 		SET_CANDIDATE(state, { data }) {
 			state.candidate = data;
